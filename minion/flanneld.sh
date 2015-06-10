ceaaaf2d3d136ce8b4a05c10e953f81c6767f98e
-10 <hushi55@126.com>
2015-06-10 17:53:15 +0800

    update subnet config

--- minion/flanneld.sh
+++ minion/flanneld.sh
@@ -5,12 +5,12 @@ KUBE_LOGTOSTDERR=false
 KUBE_LOG_LEVEL=0
 KUBE_ETCD_SERVERS=http://172.20.10.221:4001
 KUBE_BIN_DIR=/kingdee/kubernetes/logs
+KUBE_FLANNELD_SUBNET="/kingdee/kubernetes/bin/flanneld-subnet.env"
 
 cat <<EOF >/usr/lib/systemd/system/kubelet.service
 [Unit]
-Description=Kubernetes Kubelet
-After=docker.socket cadvisor.service
-Requires=docker.socket
+[Unit]
+Description=Flanneld overlay  network
 
 [Service]
 ExecStart=${KUBE_BIN_DIR}/flanneld \\
@@ -18,7 +18,7 @@ ExecStart=${KUBE_BIN_DIR}/flanneld \\
     -etcd-endpoints=${KUBE_ETCD_SERVERS} \\
     -log_dir=${KUBE_BIN_DIR} \\
     --v=${KUBE_LOG_LEVEL} \\
-    -subnet-file=${KUBE_BIN_DIR}/flanneld-subnet.env 
+    -subnet-file=${KUBE_FLANNELD_SUBNET} 
 Restart=on-failure
 
 [Install]
