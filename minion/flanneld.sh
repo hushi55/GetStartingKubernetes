8d7e45bfaf0b164aac83ca9f8f4bca00132e6951
-10 <hushi55@126.com>
2015-06-11 13:58:38 +0800

    update etcd prefix

--- minion/flanneld.sh
+++ minion/flanneld.sh
@@ -6,6 +6,8 @@ KUBE_LOG_LEVEL=0
 KUBE_ETCD_SERVERS=http://172.20.10.221:4001
 KUBE_FLANNELD_LOG=/kingdee/kubernetes/logs
 KUBE_FLANNELD_SUBNET="/kingdee/kubernetes/bin/flanneld-subnet.env"
+KUBE_FLANNELD_LAN=eth0
+KUBE_FLANNELD_ETCD_PREFIX="/kingdee/network/flanneld01"
 
 cat <<EOF >/usr/lib/systemd/system/flanneld.service
 [Unit]
@@ -13,10 +15,12 @@ Description=Flanneld overlay  network
 
 [Service]
 ExecStart=${KUBE_BIN_DIR}/flanneld \\
-    --logtostderr=${KUBE_LOGTOSTDERR} \\
+    -logtostderr=${KUBE_LOGTOSTDERR} \\
     -etcd-endpoints=${KUBE_ETCD_SERVERS} \\
     -log_dir=${KUBE_FLANNELD_LOG} \\
-    --v=${KUBE_LOG_LEVEL} \\
+    -v=${KUBE_LOG_LEVEL} \\
+    -iface=${KUBE_FLANNELD_LAN} \\
+    -etcd-prefix=${KUBE_FLANNELD_ETCD_PREFIX} \\
     -subnet-file=${KUBE_FLANNELD_SUBNET} 
 Restart=on-failure
 
