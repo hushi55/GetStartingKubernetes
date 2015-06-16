c80eeba364cb5f8b93a9c4611d1af72fe7e95da2
-10 <hushi55@126.com>
2015-06-16 17:21:31 +0800

    update

--- minion/docker-flannel.sh
+++ minion/docker-flannel.sh
@@ -1,27 +1,7 @@
 #!/bin/sh
 
-DOCKER_BRIDGE=kbr0
 DOCKER_CONFIG=/etc/sysconfig/docker
 DOCKER_FLANNELD_SUBNET=10.100.0.0/16
-
-## create Linux bridge
-brctl addbr kbr0
-cat <<EOF> /etc/sysconfig/network-scripts/ifcfg-kbr0
-DEVICE=kbr0
-ONBOOT=yes
-BOOTPROTO=static
-IPADDR=172.17.1.1
-NETMASK=255.255.255.0
-GATEWAY=172.17.1.0
-USERCTL=no
-TYPE=Bridge
-IPV6INIT=no
-EOF
-systemctl restart network
- 
-## Install Docker
-wget https://get.docker.com/builds/Linux/x86_64/docker-latest -O /usr/bin/docker
-chmod +x /usr/bin/docker
 
 cat <<EOF >$DOCKER_CONFIG
 OPTIONS=--selinux-enabled=false 
@@ -52,7 +32,7 @@ Requires=docker.socket
 [Service]
 Type=notify
 EnvironmentFile=-$DOCKER_CONFIG
-ExecStart=/usr/bin/docker -d --bridge=$DOCKER_BRIDGE -H fd:// $OPTIONS --bip=${DOCKER_FLANNELD_SUBNET}
+ExecStart=/usr/bin/docker -d -H fd:// $OPTIONS --bip=${DOCKER_FLANNELD_SUBNET}
 LimitNOFILE=1048576
 LimitNPROC=1048576
 
