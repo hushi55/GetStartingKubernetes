87cba9efc00baaf5432dd4893af23fe9d36bd1aa
-10 <hushi55@126.com>
2015-06-17 19:10:34 +0800

    update

--- centos7/init-install.sh
+++ centos7/init-install.sh
@@ -5,8 +5,12 @@ systemctl stop firewalld.service
 systemctl disable firewalld.service
 
 # install netstat
+yum install -y bridge-utils
 yum install -y net-tools
 yum install -y telnet
+yum install -y tcpdump
 yum install -y git
 yum install -y wget
 yum install -y strace
+yum install -y vim 
+yum install -y zip unzip
