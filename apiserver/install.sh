d7170037960ef90dc80b879c665828307b1e8893
-10 <hushi55@126.com>
2015-06-24 14:28:39 +0800

    add stop services

--- apiserver/install.sh
+++ apiserver/install.sh
@@ -1,5 +1,30 @@
 #!bin/sh
 
+echo "stoping per service ..."
+systemctl stop apiserver
+systemctl stop controller-manager
+systemctl stop scheduler
+systemctl stop etcd
+systemctl disable flanneld
+systemctl stop docker-bootstrap.socket
+systemctl stop docker-bootstrap.service
+systemctl stop docker.socket
+systemctl stop docker.service
+systemctl stop kubelet
+systemctl stop proxy
+
+systemctl disable apiserver
+systemctl disable controller-manager
+systemctl disable scheduler
+systemctl disable etcd
+systemctl disable flanneld
+systemctl disable docker-bootstrap.socket
+systemctl disable docker-bootstrap.service
+systemctl disable docker.socket
+systemctl disable docker.service
+systemctl disable kubelet
+systemctl disable proxy
+
 ## dir mk
 KUBE_LOG_DIR=/kingdee/kubernetes/logs
 
