69bd2093413a9441f1303a13a8f5a0d5e92870a9
-10 <hushi55@126.com>
2015-06-25 17:36:03 +0800

    add no dns install script

---
+++ minion/install_nodns.sh
@@ -0,0 +1,47 @@
+#!bin/sh
+
+echo "stoping per service ..."
+systemctl stop apiserver
+systemctl stop controller-manager
+systemctl stop scheduler
+systemctl stop etcd
+systemctl disable flanneld
+systemctl stop docker-bootstrap.socket
+systemctl stop docker-bootstrap.service
+systemctl stop docker.socket
+systemctl stop docker.service
+systemctl stop kubelet
+systemctl stop proxy
+
+systemctl disable apiserver
+systemctl disable controller-manager
+systemctl disable scheduler
+systemctl disable etcd
+systemctl disable flanneld
+systemctl disable docker-bootstrap.socket
+systemctl disable docker-bootstrap.service
+systemctl disable docker.socket
+systemctl disable docker.service
+systemctl disable kubelet
+systemctl disable proxy
+
+KUBE_LOG_DIR=/kingdee/kubernetes/logs
+
+## clean
+echo "cleaning ..."
+rm -rf /kingdee/kubernetes/logs/*
+
+## dir mk
+mkdir -p ${KUBE_LOG_DIR}
+
+echo "installing flanneld service ..."
+sh ./flanneld.sh
+
+echo "installing docker service ..."
+sh ./docker-flannel.sh
+
+echo "installing kubelet service ..."
+sh ./kubelet_nodns.sh
+
+echo "installing proxy service ..."
+sh ./proxy.sh
