6b79931ea467f81ee910886d857a35b5bfe0549b
-10 <hushi55@126.com>
2015-06-26 14:12:59 +0800

    add dns support

--- k8s-on-docker/k8s-kubelet-proxy.sh
+++ k8s-on-docker/k8s-kubelet-proxy.sh
@@ -11,6 +11,8 @@ KUBE_LOGTOSTDERR=true
 KUBE_LOG_LEVEL=0
 KUBE_MASTER=172.20.10.221:8080
 KUBE_LISTEN_ADDRESS=0.0.0.0
+KUBE_CLUSTER_DNS=10.100.100.100
+KUBE_CLUSTER_DOMAIN=k8s.cluster.local
 
 echo "========= installing docker-main kubernetes minoins ..."
 ## kubernetes master
@@ -23,6 +25,8 @@ sudo docker run --net=host -d \
 					    --v=${KUBE_LOG_LEVEL} \
 					    --address=${KUBE_LISTEN_ADDRESS} \
 						--api-servers=${KUBE_MASTER} \
+						--cluster_dns=${KUBE_CLUSTER_DNS} \\
+						--cluster_domain=${KUBE_CLUSTER_DOMAIN} \\
 					    --allow-privileged=${KUBE_ALLOW_PRIV}
 					    
 sudo docker run --net=host -d \
