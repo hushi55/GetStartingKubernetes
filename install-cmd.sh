c92e83bf80f52dbd2382341b9b52886f92fe3df9
-10 <hushi55@126.com>
2015-06-28 13:20:57 +0800

    add slave install cmd

--- install-cmd.sh
+++ install-cmd.sh
@@ -37,4 +37,7 @@ cd && rm -fr ~/GetStartingKubernetes && git clone https://github.com/hushi55/Get
 cd && rm -fr ~/GetStartingKubernetes && git clone https://github.com/hushi55/GetStartingKubernetes.git ~/GetStartingKubernetes && cd ~/GetStartingKubernetes/k8s-on-docker-Quagga/ && sh install-k8s-master.sh
 
 
+cd && rm -fr ~/GetStartingKubernetes && git clone https://github.com/hushi55/GetStartingKubernetes.git ~/GetStartingKubernetes && cd ~/GetStartingKubernetes/k8s-on-docker-Quagga/ && sh install-k8s-slave.sh
+
+
  
