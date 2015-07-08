14d5ce06b2b4cc5c25daff2b2f785ef6b80d2897
-10 <hushi55@126.com>
2015-07-08 10:56:53 +0800

    update api to v1

--- addons/install.sh
+++ addons/install.sh
@@ -36,8 +36,8 @@ kubectl create -f cluster-monitoring/kube-config/influxdb
 
 
 kubectl stop -f cluster-monitoring/kube-config/influxdb/heapster-controller.json
-kubectl stop -f cluster-monitoring/kube-config/influxdb/influxdb-grafana-controller.json
 kubectl delete -f cluster-monitoring/kube-config/influxdb/heapster-service.json
+kubectl stop -f cluster-monitoring/kube-config/influxdb/influxdb-grafana-controller.json
 kubectl delete -f cluster-monitoring/kube-config/influxdb/grafana-service.json
 kubectl delete -f cluster-monitoring/kube-config/influxdb/influxdb-service.json
 kubectl delete -f cluster-monitoring/kube-config/influxdb/influxdb-ui-service.json
