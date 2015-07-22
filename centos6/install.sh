29ea3037bfd1eb079510c4d0d080ea6dd115f959
-10 <hushi55@126.com>
2015-07-22 13:51:30 +0800

    update to wget

--- centos6/install.sh
+++ centos6/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -GET https://github.com/coreos/flannel/releases/download/v0.5.1/flannel-0.5.1-linux-amd64.tar.gz -o /tmp/flannel-0.5.1-linux-amd64.tar.gz
+wget https://github.com/coreos/flannel/releases/download/v0.5.1/flannel-0.5.1-linux-amd64.tar.gz -o /tmp/flannel-0.5.1-linux-amd64.tar.gz
 
 tar -zxvf /tmp/flannel-0.5.1-linux-amd64.tar.gz -C /tmp
 
