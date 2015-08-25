e80403cfd32dfb7dce88512d383ade75e503d045
-10 <-10@172.20.200.36>
2015-08-25 16:27:35 +0800

    add --storage-opt dm.override_udev_sync_check=true

--- product/docker-main.sh
+++ product/docker-main.sh
@@ -41,7 +41,7 @@ Requires=docker.socket
 [Service]
 Type=notify
 EnvironmentFile=-$DOCKER_CONFIG
-ExecStart=/usr/bin/docker daemon --log-level=error -H unix:///var/run/docker.sock ${OPTIONS}
+ExecStart=/usr/bin/docker daemon --log-level=error --storage-opt dm.override_udev_sync_check=true -H unix:///var/run/docker.sock ${OPTIONS}
 LimitNOFILE=1048576
 LimitNPROC=1048576
 
