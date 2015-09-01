9d99794d9aa857033d1b84f40e7a802738941f3f
-10 <-10@172.20.200.36>
2015-09-01 17:21:21 +0800

    update

--- CI/run/k8s/install.sh
+++ CI/run/k8s/install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+######################################
+##### install confd     ##############
+######################################
 confd_version = 0.10.0
 
 CONFD_BIN=/kingdee/confd/bin/
@@ -12,11 +15,16 @@ mkdir -p ${CONFD_CONF}/{templates,conf.d}
 ## download confd bin
 curl https://github.com/kelseyhightower/confd/releases/download/v${confd_version}/confd-${confd_version}-linux-amd64 -o /kingdee/confd/bin/confd
 
-cp ./conf.d/* ${CONFD_CONF}/conf.d
-cp ./conf.d/templates/master/location/location.tmpl ${CONFD_CONF}/templates
-cp ./conf.d/templates/master/upstream/upstream.tmpl ${CONFD_CONF}/templates
+#cp ./conf.d/* ${CONFD_CONF}/conf.d
+#cp ./conf.d/templates/master/location/location.tmpl ${CONFD_CONF}/templates
+#cp ./conf.d/templates/master/upstream/upstream.tmpl ${CONFD_CONF}/templates
 
-${CONFD_BIN}/confd -onetime -backend etcd -node 192.168.1.237:2379 -confdir=${CONFD_CONF}
+
+
+
+######################################
+##### mkdir tree     ##############
+######################################
 
 
 mkdir -p /usr/local/nginx/conf/conf.d/{location,stream,server}
@@ -28,3 +36,7 @@ mkdir -p /usr/local/nginx/conf/conf.d/{location,stream}/${BRANCH}/
 ## 
 sh ./nginx/generate.server.sh ${BRANCH}
 sh ./nginx/generate.confd.conf.sh ${BRANCH} ${CONFD_CONF}
+
+
+
+${CONFD_BIN}/confd -onetime -backend etcd -node 192.168.1.237:2379 -confdir=${CONFD_CONF}
