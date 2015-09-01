e1b8a04eb9a284383603b7d46c4abf0da1e5e4a7
-10 <-10@172.20.200.36>
2015-09-01 18:30:15 +0800

    update

--- CI/run/k8s/nginx/generate.confd.conf.sh
+++ CI/run/k8s/nginx/generate.confd.conf.sh
@@ -41,7 +41,7 @@ location /{{index \$urls 0}} {
  	error_page 404  /res/error/404.html;
 	error_page 500  /res/error/500.html;
     proxy_pass http://{{\$data.metadata.name}};
-    health_check;
+    #health_check;
     proxy_redirect off;
     proxy_set_header X-Real-IP \$remote_addr;
     proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
