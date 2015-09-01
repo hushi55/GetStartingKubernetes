377254d45f5fc981878c9b4dd7528588079cc95c
-10 <-10@172.20.200.36>
2015-09-01 18:04:50 +0800

    update

--- CI/run/k8s/nginx/generate.confd.conf.sh
+++ CI/run/k8s/nginx/generate.confd.conf.sh
@@ -5,6 +5,58 @@ confd_conf_dir=$2
 
 mkdir -p /usr/local/nginx/conf/conf.d/server
 
+
+
+cat <<EOF >${confd_conf_dir}/templates/${branch}-upstream.tmpl
+
+{{\$endpoints := getvs "/registry/services/endpoints/kingdee-${branch}/*"}}
+{{range \$spec := \$endpoints}}
+{{\$data := json \$spec}}
+{{ if \$data.subsets }}
+
+	upstream {{\$data.metadata.name}} {
+		{{range \$si, \$se := \$data.subsets}}
+		{{range  \$ai, \$ae := \$se.addresses}}
+	    server {{\$ae.ip}}:10091
+		{{ end }}
+		{{ end }}
+	}
+
+{{ end }}
+{{ end }}
+
+EOF
+
+cat <<EOF >${confd_conf_dir}/templates/${branch}-location.tmpl
+
+{{\$endpoints := getvs "/registry/services/endpoints/kingdee-${branch}/*"}}
+{{range \$spec := \$endpoints}}
+{{\$data := json \$spec}}
+{{ if \$data.subsets }}
+{{\$urls := split \$data.metadata.name "-"}}
+location /{{index \$urls 0}} {
+ 	proxy_intercept_errors on;
+ 	error_page 401  /res/error/500.html;
+ 	error_page 403  /res/error/500.html;
+ 	error_page 404  /res/error/404.html;
+	error_page 500  /res/error/500.html;
+    proxy_pass {{\$data.metadata.name}};
+    proxy_redirect off;
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header Host \$http_host;
+}
+
+{{ end }}
+{{ end }}
+
+EOF
+
+
+#############################################################
+#######  first  tmp
+#############################################################
+
 cat <<EOF >${confd_conf_dir}/conf.d/${branch}-locations.toml
 
 [template]
@@ -39,47 +91,3 @@ EOF
 #############################################################
 #############################################################
 
-cat <<EOF >${confd_conf_dir}/templates/${branch}-location.tmpl
-
-{{\$endpoints := getvs "/registry/services/endpoints/kingdee-${branch}/*"}}
-{{range \$spec := \$endpoints}}
-{{\$data := json \$spec}}
-{{ if \$data.subsets }}
-{{\$urls := split \$data.metadata.name "-"}}
-location /{{index \$urls 0}} {
- 	proxy_intercept_errors on;
- 	error_page 401  /res/error/500.html;
- 	error_page 403  /res/error/500.html;
- 	error_page 404  /res/error/404.html;
-	error_page 500  /res/error/500.html;
-    proxy_pass {{\$data.metadata.name}};
-    proxy_redirect off;
-    proxy_set_header X-Real-IP \$remote_addr;
-    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-    proxy_set_header Host \$http_host;
-}
-
-{{ end }}
-{{ end }}
-
-EOF
-
-cat <<EOF >${confd_conf_dir}/templates/${branch}-upstream.tmpl
-
-{{\$endpoints := getvs "/registry/services/endpoints/kingdee-${branch}/*"}}
-{{range \$spec := \$endpoints}}
-{{\$data := json \$spec}}
-{{ if \$data.subsets }}
-
-	upstream {{\$data.metadata.name}} {
-		{{range \$si, \$se := \$data.subsets}}
-		{{range  \$ai, \$ae := \$se.addresses}}
-	    server {{\$ae.ip}}:10091
-		{{ end }}
-		{{ end }}
-	}
-
-{{ end }}
-{{ end }}
-
-EOF
