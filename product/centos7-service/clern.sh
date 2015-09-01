8e93db7e3288cc05428b4e32194492339a02eb13
-10 <-10@172.20.200.36>
2015-09-01 16:12:31 +0800

    add clean docker none images

--- product/centos7-service/clern.sh
+++ product/centos7-service/clern.sh
@@ -5,6 +5,9 @@ echo "========= docker cleaning exited and dead contain ..."
 #docker ps -a | grep -E 'Exited|Dead' | awk '{print $1}'  | xargs --no-run-if-empty docker rm -f
 docker ps -a | grep -v CONTAINER | awk '{print $1}' | xargs --no-run-if-empty docker inspect --format '{{ .Id }} {{ .State.Running }}' | grep  'false' | awk '{print $1}'  | xargs --no-run-if-empty docker rm -f
 
+echo "========= docker clean images"
+docker images | grep '<none>' | awk '{print $3}' | xargs --no-run-if-empty docker rmi -f
+
 
 
 echo "========= cleaning kubernetes soft links ..."
