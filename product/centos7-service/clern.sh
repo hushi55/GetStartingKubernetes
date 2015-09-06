1be3e53436863cee9ed93a7c94ac33d1d8611d30
-10 <-10@172.20.200.36>
2015-09-06 11:12:20 +0800

    update

--- product/centos7-service/clern.sh
+++ product/centos7-service/clern.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
+redEcho() {
+    [ -c /dev/stdout ] && {
+        # if stdout is console, turn on color output.
+        echo -ne "\033[1;31m"
+        echo -n "$@"
+        echo -e "\033[0m"
+    } || echo "$@"
+}
 
-echo "========= docker cleaning exited and dead contain ..."
+redEcho "========= docker cleaning exited and dead contain ..."
 #docker ps -a | grep -E 'Exited|Dead' | awk '{print $1}'  | xargs --no-run-if-empty docker rm -f
 docker ps -a | grep -v CONTAINER | awk '{print $1}' | xargs --no-run-if-empty docker inspect --format '{{ .Id }} {{ .State.Running }}' | grep  'false' | awk '{print $1}'  | xargs --no-run-if-empty docker rm -f
 
-echo "========= docker clean images"
+redEcho "========= docker clean images"
 docker images | grep '<none>' | awk '{print $3}' | xargs --no-run-if-empty docker rmi -f
-
-
-
-echo "========= cleaning kubernetes soft links ..."
-yum install -y symlinks
-symlinks -d /var/log/containers
