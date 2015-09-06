0723479bb831d6d52ec628b34337ac909446d442
-10 <-10@172.20.200.36>
2015-09-06 10:05:12 +0800

    update

--- registry/run.sh
+++ registry/run.sh
@@ -3,8 +3,11 @@
 docker run -d -p 5000:5000  -v /kingdee/docker/registry/images/:/var/lib/registry registry:2
 
 docker run -d -p 5000:5000 --restart=always -v /kingdee/docker/registry/images/:/var/lib/registry registry:2
+docker run -d -p 5000:5000 --restart=always registry:2
+docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
 
 
-
-docker run -p 8080:8080 atcol/docker-registry-ui
+docker run -d --restart=always  --privileged=true --net=host index.alauda.cn/georce/router
+docker run -d -p 8080:8080 --restart=always atcol/docker-registry-ui
+docker run -d -p 9087:80 --restart=always -e ENV_DOCKER_REGISTRY_HOST=172.20.10.220 -e ENV_DOCKER_REGISTRY_PORT=5000 konradkleine/docker-registry-frontend
