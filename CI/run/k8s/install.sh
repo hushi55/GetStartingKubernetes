d7112b9da8d89ab50f26509f381e2de1de8068c2
-10 <-10@172.20.200.36>
2015-09-15 15:33:29 +0800

    update

--- CI/run/k8s/install.sh
+++ CI/run/k8s/install.sh
@@ -66,3 +66,5 @@ sh ${BASE_DIR}/nginx/generate.server.tmpl.sh ${BRANCH} ${CONFD_CONF}
 
 #nohup /kingdee/confd/bin/confd -interval=60 -backend etcd -node 192.168.1.237:2379 -confdir=/kingdee/confd/conf/ -log-level="debug" -watch=true > /kingdee/confd/logs/confd.log 2>&1 &
 kill -9 `pgrep confd` && nohup /kingdee/confd/bin/confd -interval=60 -backend etcd -node 192.168.1.237:2379 -confdir=/kingdee/confd/conf/ -watch=true > /kingdee/confd/logs/confd.log 2>&1 &
+
+echo "install successed"
