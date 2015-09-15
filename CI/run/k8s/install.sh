fd40acc5d6dcde788059a114ad1da4593074e324
-10 <-10@172.20.200.36>
2015-09-15 16:29:12 +0800

    add return value

--- CI/run/k8s/install.sh
+++ CI/run/k8s/install.sh
@@ -68,3 +68,5 @@ sh ${BASE_DIR}/nginx/generate.server.tmpl.sh ${BRANCH} ${CONFD_CONF}
 kill -9 `pgrep confd` && nohup /kingdee/confd/bin/confd -interval=60 -backend etcd -node 192.168.1.237:2379 -confdir=/kingdee/confd/conf/ -watch=true > /kingdee/confd/logs/confd.log 2>&1 &
 
 echo "install successed"
+
+return 0
